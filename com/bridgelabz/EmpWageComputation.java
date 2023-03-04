e7bfbb69b0ef78aa634cd6329569e63a149cf5cc
ASHU-code06 <ashuc306@gmail.com>
2023-03-05 02:42:40 +0530

    Calculated daily wage for parttime Employee and fulltime employee using switch case

--- com/bridgelabz/EmpWageComputation.java
+++ com/bridgelabz/EmpWageComputation.java
@@ -1,30 +1,35 @@
 package com.bridgelabz;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class EmpWageComputation {
     public static void main(String[] args) {
-        int r;
+        EmpWageComputation empWageComputation = new EmpWageComputation();
+        empWageComputation.check();
+    }
+     void check(){
         int wagePerHour = 20;
         int fullDayHour = 8;//for fulltime Employee
         int fulltimeDailyWage ;
         int partTimeHour = 4;//for parttime Employee
         int parttimeDailyWage ;
-        Random ran = new Random();
-        r= ran.nextInt(3);
-        System.out.println("random number is  "   +r);
-        if(r==0){
-            System.out.println("Employee is absent");
-        }
-        if(r==1){
-            System.out.println("Employee is present and fulltime");
-            fulltimeDailyWage = fullDayHour * wagePerHour;
-            System.out.println("Daily wage of a fulltime employee is "+fulltimeDailyWage+" rs");
-        }
-        if (r==2){
-            System.out.println("Employee is present and parttime ");
-            parttimeDailyWage = partTimeHour * wagePerHour ;
-            System.out.println("Daily Wage of a parttime employee is "+parttimeDailyWage+" rs");
+        Scanner myinput = new Scanner(System.in);
+        System.out.println("Enter 1 for fulltime wage else Enter 2 for parttime wage calculation");
+        int select = myinput.nextInt();
+        switch(select){
+            case 1:
+                System.out.println("Employee is present and fulltime");
+                fulltimeDailyWage = fullDayHour * wagePerHour;
+                System.out.println("Daily wage of a fulltime employee is "+fulltimeDailyWage+" rs");
+                break;
+            case 2:
+                System.out.println("Employee is present and parttime ");
+                parttimeDailyWage = partTimeHour * wagePerHour ;
+                System.out.println("Daily Wage of a parttime employee is "+parttimeDailyWage+" rs");
+                break;
+            default:
+                System.out.println("Employee is absent");
         }
     }
 }
